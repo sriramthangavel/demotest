3f2c7797fc0ff9c11b68365c2a4c53e775a62565
unknown <sriramsmrg@gmail.com>
2021-12-10 11:40:49 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var request= new XMLHttpRequest();
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(request.response);
+   console.log(data);
+       data.forEach(ele=>(console.log(ele.name,ele.capital,ele.flag)));
+     }
+
+
+
+
